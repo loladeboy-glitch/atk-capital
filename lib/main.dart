ee711bc0cb3f088bf3bad59720205bc7ef888599
loladeboy-glitch <loladeboy@gmail.com>
2026-07-13 01:49:14 +0100

    Update main.dart
    Add bottom navigation with Portfolio + Signals

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/portfolio_screen.dart';
+import 'screens/signal_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,46 @@ class MyApp extends StatelessWidget {
       title: 'ATK Capital',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: const PortfolioScreen(),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const PortfolioScreen(),
+    const SignalScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        selectedItemColor: Colors.green,
+        backgroundColor: Colors.black,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet),
+            label: 'Portfolio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.trending_up),
+            label: 'Signals',
+          ),
+        ],
+      ),
     );
   }
 }
