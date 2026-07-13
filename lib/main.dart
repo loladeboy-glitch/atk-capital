555685fc0b591c8176e49709e93c99d159f0ebef
loladeboy-glitch <loladeboy@gmail.com>
2026-07-13 01:32:32 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,20 @@
 import 'package:flutter/material.dart';
-import 'screens/signal_screen.dart';
 import 'screens/portfolio_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ATK Capital',
-      theme: ThemeData.dark(),
-      home: HomeScreen(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _currentIndex = 0;
-  final screens = [
-    SignalScreen(),
-    PortfolioScreen(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: screens[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        backgroundColor: Colors.black,
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.grey,
-        onTap: (index) => setState(() => _currentIndex = index),
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.signal_cellular_alt), label: 'Signal'),
-          BottomNavigationBarItem(icon: Icon(Icons.account_balance_wallet), label: 'Portfolio'),
-        ],
-      ),
+      theme: ThemeData.dark(),
+      home: const PortfolioScreen(),
     );
   }
 }
